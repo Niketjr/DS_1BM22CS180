6aa86877dd0cc596a3e508e2219cd4413176ce51
Niket Dugar <143904117+Niketjr@users.noreply.github.com>
2024-01-03 22:56:43 +0530

    Update dynamic_memory_allocation.c

--- 21-12-23 codes/dynamic_memory_allocation.c
+++ 21-12-23 codes/dynamic_memory_allocation.c
@@ -2,29 +2,31 @@
 
 int main()
 {
-int* ptr;
-int n;
-printf("Enter number of elements:");
-scanf("%d",&n);
-ptr = (int*)malloc(n * sizeof(int));
-if (ptr == NULL) {
-printf("Memory not allocated.\n");
-exit(0);
-}
-else {
-printf("Memory successfully allocated using malloc.\n");
-printf("Enter elements of array\n");
-for (int i = 0; i < n; i++) {
-scanf("%d", &ptr[i]);
-}
-printf("The elements of the array are: ");
-for (int i = 0; i < n; i++) {
-printf("%d, ", ptr[i]);
-}
-}
-free(ptr);
-printf("Memory Freed\n");
-ptr = (int*)calloc(n, sizeof(int));
+    int* ptr;
+    int n;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    ptr = (int*)malloc(n * sizeof(int));
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+        printf("Memory successfully allocated using malloc.\n");
+        printf("Enter elements of array\n");
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &ptr[i]);
+        }
+        printf("The elements of the array are: ");
+        for (int i = 0; i < n; i++) {
+            printf("%d, ", ptr[i]);
+        }
+    }
+    
+    free(ptr);
+    printf("Memory Freed\n");
+    
+    ptr = (int*)calloc(n, sizeof(int));
     if (ptr == NULL) {
         printf("Memory not allocated.\n");
         exit(0);
@@ -39,6 +41,7 @@ ptr = (int*)calloc(n, sizeof(int));
         for (int i = 0; i < n; i++) {
             printf("%d, ", ptr[i]);
         }
+        
         int n1;
         printf("Enter new size for realloc\n");
         scanf("%d", &n1);
