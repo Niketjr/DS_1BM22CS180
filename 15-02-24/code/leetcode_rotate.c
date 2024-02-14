c64178e8370e123d56344b44bf63a60f8c58d11b
Niket Dugar <143904117+Niketjr@users.noreply.github.com>
2024-02-14 23:36:54 +0530

    Create leetcode_rotate.c

---
+++ 15-02-24/code/leetcode_rotate.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* rotateRight(struct ListNode* head, int k) {
+    struct ListNode* last, *slast;
+    while(head==NULL || k==0){
+        return head;
+    }
+    last = head;
+    int length = 1;
+    while(last->next){
+        last = last->next;
+        length++;
+    }
+    k = k % length;
+    slast = head;
+    for(int i=0; i<length-k-1; i++){
+        slast = slast->next;
+    }
+    last->next = head;
+    head = slast->next;
+    slast->next = NULL;
+    
+   
+    return head;
+}
